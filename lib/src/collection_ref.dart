371159fe985774a8d7fe6679159e9c374b164436
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-12-25 17:59:22 +0900

    refactor: refactor collection_ref and add doc comments

--- lib/src/collection_ref.dart
+++ lib/src/collection_ref.dart
@@ -4,7 +4,8 @@ import 'package:rxdart/rxdart.dart';
 import '../geoflutterfire_plus.dart';
 import 'math.dart';
 
-/// ドキュメントスナップショットと中心からの距離をまとめたクラス。
+/// Class to handle cloud_firestore [DocumentSnapshot]
+/// with distance from given center in kilometers.
 class GeoDocumentSnapshot<T> {
   GeoDocumentSnapshot({
     required this.documentSnapshot,
@@ -18,35 +19,35 @@ class GeoDocumentSnapshot<T> {
   final double distanceFromCenterInKm;
 }
 
-///
 class GeoCollectionRef<T> {
   GeoCollectionRef(this._collectionReference);
 
-  ///
+  /// Typed [Query] of target collection.
   final Query<T> _collectionReference;
 
+  /// Detection range buffer when not strict mode.
+  static const _detectionRangeBuffer = 1.02;
+
+  /// Notifies of geo query results by given condition.
+  ///
+  /// - [center] Center point of detection.
+  /// - [radiusInKm] Detection range in kilometers.
+  /// - [strictMode] Whether to filter documents strictly within
+  /// the bound of given radius.
   Stream<List<GeoDocumentSnapshot<T>>> within({
     required GeoFirePoint center,
-    required double radius,
+    required double radiusInKm,
     required String field,
     required GeoPoint Function(T obj) geopointFromObject,
-    // TODO: strictMode の説明を書く
     bool strictMode = false,
   }) {
-    // int: geohash の精度（桁数）
-    final precision = geohashDigitsFromRadius(radius);
-
-    // String: 中心の geohash
-    final centerGeoHash = center.geohash.substring(0, precision);
-
-    // List<String>: 検出範囲の geohash 一覧（中心含む）
+    final precisionDigits = geohashDigitsFromRadius(radiusInKm);
+    final centerGeoHash = center.geohash.substring(0, precisionDigits);
     final geohashes = [
       ...neighborGeohashesOf(geohash: centerGeoHash),
       centerGeoHash,
     ];
 
-    // Iterable of
-    // geohash <= x <= geohash~
     final collectionStreams = geohashes
         .map(
           (geohash) => _collectionReference
@@ -58,9 +59,6 @@ class GeoCollectionRef<T> {
         )
         .toList();
 
-    // ここが何をやっているのかわからない
-    // 指定された複数の Stream (List<Stream>) を、第 2 引数の combiner 関数によって
-    // ひとつの Stream にまとめている
     final mergedCollectionStreams = Rx.combineLatest(
       collectionStreams,
       (values) => [
@@ -68,25 +66,22 @@ class GeoCollectionRef<T> {
       ],
     );
 
-    // Stream<List<DistanceDocSnapshot<T>>>
     final filtered = mergedCollectionStreams.map((queryDocumentSnapshots) {
       final mappedList = queryDocumentSnapshots.map((queryDocumentSnapshot) {
         final exists = queryDocumentSnapshot.exists;
         if (!exists) {
           return null;
         }
-        final data = queryDocumentSnapshot.data();
-        final geopoint = geopointFromObject(data);
-
-        // 中心と指定した緯度・経度の点との間の距離 (km)
-        final kilometers = center.distanceBetweenInKm(
-          latitude: geopoint.latitude,
-          longitude: geopoint.longitude,
+        final fetchedData = queryDocumentSnapshot.data();
+        final fetchedGeopoint = geopointFromObject(fetchedData);
+        final distanceFromCenterInKm = center.distanceBetweenInKm(
+          latitude: fetchedGeopoint.latitude,
+          longitude: fetchedGeopoint.longitude,
         );
 
         return GeoDocumentSnapshot(
           documentSnapshot: queryDocumentSnapshot,
-          distanceFromCenterInKm: kilometers,
+          distanceFromCenterInKm: distanceFromCenterInKm,
         );
       }).toList();
 
@@ -95,7 +90,7 @@ class GeoCollectionRef<T> {
               (doc) =>
                   doc != null &&
                   doc.distanceFromCenterInKm <=
-                      radius * 1.02, // buffer for edge distances;
+                      radiusInKm * _detectionRangeBuffer,
             )
           : mappedList;
       return nullableFilteredList.whereType<GeoDocumentSnapshot<T>>().toList()
