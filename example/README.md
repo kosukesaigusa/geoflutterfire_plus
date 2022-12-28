9de94917fd69da42669a70a4531465cf79451dac
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-12-28 11:08:57 +0900

    feat: add example directory

--- example/README.md
+++ example/README.md
@@ -1,6 +1,6 @@
 # Examples
 
-## Pure Flutter
+## Simple
 
 ## Riverpod
 
---
+++ examples/simple/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sample app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MapPage(),
+    );
+  }
+}
+
+class MapPage extends StatelessWidget {
+  const MapPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
