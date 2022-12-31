471ecab54b5344a57b2bc7b8f9012ce2ce99874a
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-12-31 17:41:17 +0900

    chore: update doc comments

--- example/lib/advanced/utils.dart
+++ example/lib/advanced/utils.dart
@@ -4,8 +4,6 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'entity.dart';
 
 /// Tokyo Station location for demo.
-/// You can get latitude and longitude from this site:
-/// https://www.geocoding.jp/
 const tokyoStation = LatLng(35.681236, 139.767125);
 
 /// Typed reference to the collection where the location data is stored.
