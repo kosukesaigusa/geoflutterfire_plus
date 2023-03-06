16d3c5d2ad087c75277e16660c7e39a1aed0194f
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-03-06 21:15:52 +0900

    feat: remove equatable package from dependency and remove Coordinate class

--- test/math_test.dart
+++ test/math_test.dart
@@ -1,14 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:geoflutterfire_plus/geoflutterfire_plus.dart';
 import 'package:geoflutterfire_plus/src/math.dart';
 
 void main() {
   test('Test encode method by known datasets', () {
-    final knownDatasets = <Coordinates, String>{
+    final knownDatasets = <GeoPoint, String>{
       // Tokyo Station
-      const Coordinates(35.681236, 139.767125): 'xn76urx66',
+      const GeoPoint(35.681236, 139.767125): 'xn76urx66',
       // Shibuya Station
-      const Coordinates(35.658034, 139.701636): 'xn76fgreh',
+      const GeoPoint(35.658034, 139.701636): 'xn76fgreh',
     };
     for (final dataset in knownDatasets.entries) {
       final geohash = encode(
