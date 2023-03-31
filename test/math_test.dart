fefb01e18aa3de0fbff753a0f2fda77ded96530a
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-04-01 01:39:05 +0900

    test: add missing test for geohashDigitsFromRarius method in math.dart

--- test/math_test.dart
+++ test/math_test.dart
@@ -1,21 +1,16 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:geoflutterfire_plus/src/math.dart';
 
 void main() {
-  test('Test encode method by known datasets', () {
-    final knownDatasets = <GeoPoint, String>{
-      // Tokyo Station
-      const GeoPoint(35.681236, 139.767125): 'xn76urx66',
-      // Shibuya Station
-      const GeoPoint(35.658034, 139.701636): 'xn76fgreh',
-    };
-    for (final dataset in knownDatasets.entries) {
-      final geohash = encode(
-        latitude: dataset.key.latitude,
-        longitude: dataset.key.longitude,
-      );
-      expect(geohash, dataset.value);
-    }
+  test('Test geohashDigitsFromRadius method.', () {
+    expect(geohashDigitsFromRadius(0.00477), 9);
+    expect(geohashDigitsFromRadius(0.0382), 8);
+    expect(geohashDigitsFromRadius(0.153), 7);
+    expect(geohashDigitsFromRadius(1.22), 6);
+    expect(geohashDigitsFromRadius(4.89), 5);
+    expect(geohashDigitsFromRadius(39.1), 4);
+    expect(geohashDigitsFromRadius(156), 3);
+    expect(geohashDigitsFromRadius(1250), 2);
+    expect(geohashDigitsFromRadius(1251), 1);
   });
 }
